249145e94bdae7f625c4c59dfc6bf97d2d58cf2e
henrique-noronha <henriquenoronha020@gmail.com>
2025-04-29 14:19:40 -0300

    loginAdm

---
+++ medfinder/app/admin/register-professional.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function RegisterProfessional() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Cadastro de Profissional (em construção)</Text>
+    </View>
+  );
+}
